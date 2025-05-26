bef0eaf5ff66b50a40c1c590a85f54d3aa68f94d
László <93830438+Halalos@users.noreply.github.com>
2025-05-26 09:10:19 +0200

    Thank You

---
+++ thankyou.php
@@ -0,0 +1,15 @@
+<?php
+  include('partials/header.php');
+?>
+<main>
+    <section class="container">
+      <div class="row">
+        <div class="col-100 text-center">
+          <h1>Ďakujeme  za vyplnenie formulára</h1>
+        </div>
+      </div>
+    </section>
+</main>
+<?php
+  include('partials/footer.php');
+?> 
