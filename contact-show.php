f71218e73a8bd6235e1379f2a9f4f0e8389517a7
László <93830438+Halalos@users.noreply.github.com>
2025-05-26 09:07:47 +0200

    contact-show

--- contact-show.php
+++ contact-show.php
@@ -1,19 +1,25 @@
 <?php
-    include('partials/header.php');
-    $db = new Database();
-    $contact = new Contact($db);
+include('partials/header.php');
+$db = new Database();
+$contact = new Contact($db);
 
-    if(isset($_GET['id'])){
-        //echo $_GET['id'];  
-        $id = $_GET['id'];
-        $contactData = $contact->show($id);  
-    }
+if(isset($_GET['id'])){
+    //print_r($_GET['id']);
+    $id = $_GET['id'];
+    $contactData = $contact->show($id);
+    //print_r($contactData);
+}
+
 ?>
-<h1>Detail spravy</h1>
-<p>Meno: <?php echo $contactData['name'] ?></p>
-<p>Email: <?php echo $contactData['email'] ?></p>
-<p>Message: <?php echo $contactData['message'] ?></p>
-<a href="admin.php">Naspat do admin rozhrania</a>
+
+<section class="container">
+    <h1>Contact show</h1>
+    <p>Name: <?php echo($contactData['name']);?></p>
+    <p>Email: <?php echo($contactData['email']);?></p>
+    <p>Message: <?php echo($contactData['message']);?></p>
+    <a href="admin.php">Back to Contacts</a>
+</section>
+
 <?php
-    include('partials/footer.php');
+include('partials/footer.php');
 ?>
