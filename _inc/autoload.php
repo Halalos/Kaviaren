2017622429f3a15d2bfaf50c0936d2c15df292ef
László <93830438+Halalos@users.noreply.github.com>
2025-05-26 09:03:40 +0200

    autoload.php

--- _inc/autoload.php
+++ _inc/autoload.php
@@ -1,6 +1,7 @@
 <?php
-// Načítanie triedy Qna
+include_once('_inc/Menu.php');
 require_once('_inc/Database.php');
 include_once('_inc/Contact.php');
-
+include_once('_inc/Authenticate.php');
+include_once('_inc/User.php');
 ?>
